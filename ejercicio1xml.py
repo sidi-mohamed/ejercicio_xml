2976b8e807e1c484afcce75dddf1a5e28385720d
root <root@debian>
2015-02-25 10:09:18 +0100

    he modificado el fichero ejercicio1xml.py

--- ejercicio1xml.py
+++ ejercicio1xml.py
@@ -7,8 +7,8 @@ from lxml import etree
 arbol=etree.parse('perros.xml')
 nombres = arbol.findall ('animal/descripcion')
 for nombre in nombres :
-	if nombre==PITBULL TERRIER
-	print nombre.text
+	if nombre==PITBULL TERRIER:
+		print nombre.text
 
 
 
